dc94caec38134c648f4169ac26aca4b7c1accfa5
= <firmanazhar14@gmail.com>
2025-01-14 08:39:16 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_packages_library/hello.dart';
+
+void main() {
+  print(sayHello('Kenyil'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,5 @@
+library dart_packages_library;
+
+export 'src/say_hello.dart'; //export all
+export 'src/customer.dart'
+    show Customer, Category, Product; //export spesific code
